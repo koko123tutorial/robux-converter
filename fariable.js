8ed057e6bd45fabf457c23627feccb138f9f2a8a
koko123tutorial <koko123tutorial@gmail.com>
2022-05-31 22:45:32 +0700

    first commit

---
+++ fariable.js
@@ -0,0 +1,27 @@
+const robux = (value) => {
+    let inputRobux = document.getElementById("robux")
+    let outputRupiah = document.getElementById("rupiah")
+    inputRobux.value = value
+
+    const fixedPrice = 187.79
+    let robux = value * fixedPrice
+    console.log('robux bernilai = ', robux)
+    
+    let price = String(robux).split("").reverse()
+    console.log('original price => ', price)
+    
+    for(let i = 0; i < price.length; i++){
+        if((i + 1) % 3 == 0 && i !== price.length -1){
+            price[i] = `.${price[i]}`
+        }
+    }
+    console.log('converter price => ', price)
+    const totalPrice = price.reverse().join("");
+    console.log('total price => ', totalPrice);
+
+    if(inputRobux !== ""){
+        outputRupiah.value = `Rp. ${totalPrice}`
+    }else {
+        outputRupiah.value = ""
+    }
+}
